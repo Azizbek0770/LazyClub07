8668795d74e8e8991a4076d6e36fb55ac6b978bf
Baxtiyorov Azizbek <https://github.com/Azizbek0770/LazyClub07.git>
2024-05-29 12:19:36 +0500

    asw

--- FrontEnd/src/components/register.jsx
+++ FrontEnd/src/components/register.jsx
@@ -110,7 +110,6 @@ const RegisterPage = () => {
           <option value="">Select Gender</option>
           <option value="male">Male</option>
           <option value="female">Female</option>
-          <option value="other">Other</option>
         </select>
         <input
           type="password"
